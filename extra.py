07ca7bb8eefe0644a13cc0b5960b9d8499f9d04b
EmiliaHorton12 <emiliavictoriah@gmail.com>
2025-02-20 00:12:39 +0100

    extra_lab_done

---
+++ extra.py
@@ -0,0 +1,16 @@
+# Your code here
+# name, age, adress, salary and expenses = input
+# calculation of how much money they will have left after expenses
+# store age, salary and expeses are stored as integers or floats, round the remining money to one decimal
+# remaining money <= 500
+# print(""*name*, who is *age* years old, and lives in *address* has *remaining_salary* dollars left from her salary after expenses. It is *is_salary_good* that she has more than $500 left."")
+name = input(" Name: ")
+print = ("Name: ", name)
+age = input("Age: ")
+print("Age recorded:", age)
+address = input("Address: ")
+print("Address recorded:", address)
+salary = input("Salary: ")
+print("Salary recorded:", salary)
+expenses = input("Expenses: ")
+print("Expenses recorded:", expenses)
